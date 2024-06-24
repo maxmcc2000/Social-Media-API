94134a80405f5bc0a3b4bfc6ffc484e24e590703
LarryDevinCarter <larrydevincarter@gmail.com>
2024-06-24 15:09:09 -0500

    added Error handling infrastructure

---
+++ src/main/java/com/cooksys/app/controllers/advice/AppControllerAdvice.java
@@ -0,0 +1,9 @@
+package com.cooksys.app.controllers.advice;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@ControllerAdvice(basePackages = {"com.cooksys.app"})
+@ResponseBody
+public class AppControllerAdvice {
+}
