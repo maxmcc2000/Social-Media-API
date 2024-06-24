90519b9f9bcbec0390d6e2d3168ced472d1351c9
Max McClintock <maxmcclintock@Maxs-MBP.lan>
2024-06-24 18:39:12 -0500

    added ValidateService, VaildateServiceImpl, and ValidateController

--- src/main/java/com/cooksys/app/controllers/ValidateController.java
+++ src/main/java/com/cooksys/app/controllers/ValidateController.java
@@ -1,12 +1,14 @@
 package com.cooksys.app.controllers;
 
+import com.cooksys.app.servies.ValidateService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/user") //shouldn't this be "/users" to match the endpoint documentation in README? -Max
-public class UserController {
+@RequestMapping("/validate")
+public class ValidateController {
 
+    private final ValidateService validateService;
 }
