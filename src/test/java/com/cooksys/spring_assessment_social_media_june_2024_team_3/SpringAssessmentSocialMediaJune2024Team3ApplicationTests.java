9322ed487fc407d9f2c2365441f2392a28869a6b
Max McClintock <maxmcclintock@Maxs-MBP.lan>
2024-06-24 11:39:44 -0500

    Generated Spring project with all necessary dependencies. Added Mapstruct dependency and Maven build was successful.

---
+++ src/test/java/com/cooksys/spring_assessment_social_media_june_2024_team_3/SpringAssessmentSocialMediaJune2024Team3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cooksys.spring_assessment_social_media_june_2024_team_3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAssessmentSocialMediaJune2024Team3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
