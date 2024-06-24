32661b8715d538ca19bb3052eedd8849f9801d58
BuildTools <unconfigured@null.spigotmc.org>
2024-06-24 17:43:08 -0500

    Created Hashtage Repository

---
+++ src/main/java/com/cooksys/app/repositories/HashtagRepository.java
@@ -0,0 +1,11 @@
+package com.cooksys.app.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.cooksys.app.entities.Hashtag;
+
+@Repository
+public interface HashtagRepository extends JpaRepository<Hashtag, Long>{
+
+}
