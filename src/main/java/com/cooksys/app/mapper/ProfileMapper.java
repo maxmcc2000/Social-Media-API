9aba759fcc116e30b5aebfa649d0ce128bc965ad
Max McClintock <maxmcclintock@Maxs-MBP.lan>
2024-06-24 16:30:17 -0500

    added CredentialsMapper, ProfileMapper, and HashtagMapper

---
+++ src/main/java/com/cooksys/app/mapper/ProfileMapper.java
@@ -0,0 +1,13 @@
+package com.cooksys.app.mapper;
+
+import com.cooksys.app.dtos.ProfileDto;
+import com.cooksys.app.entities.Profile;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface ProfileMapper {
+
+    ProfileDto entityToDto(Profile);
+
+    Profile DtoToEntity(ProfileDto);
+}
