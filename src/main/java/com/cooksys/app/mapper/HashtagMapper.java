a687864f3c5f3c5faa67789279a610f11d96d2cd
BuildTools <unconfigured@null.spigotmc.org>
2024-06-24 19:27:23 -0500

    Adjusted Hashtag mapper to support Hashtag/DTOs

--- src/main/java/com/cooksys/app/mapper/HashtagMapper.java
+++ src/main/java/com/cooksys/app/mapper/HashtagMapper.java
@@ -1,12 +1,14 @@
 package com.cooksys.app.mapper;
 
+import com.cooksys.app.dtos.HashtagRequestDto;
+import com.cooksys.app.dtos.HashtagResponseDto;
 import com.cooksys.app.entities.Hashtag;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
 public interface HashtagMapper {
-    HashtagDto entityToDto(Hahstag);
+	HashtagResponseDto entityToDto(Hashtag entity);
 
-    Hashtag DtoToEntity(HashtagDto);
+    Hashtag dtoToEntity(HashtagRequestDto hashtageRequestDto);
 
 }
